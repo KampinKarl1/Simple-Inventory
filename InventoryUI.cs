8e7f91e86c13bafa4f92b3ae852398aa1b19115e
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2020-08-30 00:24:29 +0200

    Update InventoryUI.cs
    Removed old way of updating slots

--- InventoryUI.cs
+++ InventoryUI.cs
@@ -34,41 +34,5 @@ namespace SimpleInventory.UI
             itemSlots[_index].gameObject.SetActive(true);
             itemSlots[_index].UpdateSlotUI(item, count, buttonAction);
         }
-
-        /*protected override void UpdateItemSlots()
-        {
-            UnityAction buttonAction;
-
-            int index = 0;
-            //Iterate on each slot
-            foreach (KeyValuePair<Item, int> kvp in inventory.GetInventory)
-            {
-                Item item = kvp.Key;
-                int count = kvp.Value;
-
-                if (count == 0)
-                {
-                    itemSlots[index].gameObject.SetActive(false);
-                    continue;
-                }
-
-
-                buttonAction = new UnityAction(delegate
-               {
-                   inventory.RemoveFromInventory(item, 1);
-                   item.DoItemStuff(); 
-               });
-
-                itemSlots[index].UpdateSlotUI(item, count, buttonAction);
-                itemSlots[index].gameObject.SetActive(true);
-
-                index++;
-            }
-
-            for (int i = index; i < itemSlots.Length; i++)            
-                itemSlots[i].gameObject.SetActive(false);           
-        }*/
-
-
     }
 }
