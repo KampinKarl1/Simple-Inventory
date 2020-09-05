0d890b9713e36eb3deb227a851cc4125f13c7a68
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2020-09-05 20:08:48 +0200

    Added mining.
    MiningUI is built for an object that has a background image with a child image set to fill mode and another image that will hold a resource's sprite.
    
    Miner works with MiningUI and Inventory.
    
    Mineable uses the Item class for its resources.

---
+++ Mineable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mineable : MonoBehaviour
+{
+    Miner currentMiner = null;
+
+    [SerializeField] private SimpleInventory.Item resource = null;
+
+    private bool beingMined = false;
+
+    [SerializeField] private int minResources = 1;
+    [SerializeField] private int maxResources = 5;
+    private int resourcesRemaining = 0;
+
+    [SerializeField] private float miningTime = 1.0f;
+    private float countdown = 0f;
+
+    public SimpleInventory.Item Resource => resource;
+    public float MiningProgress => countdown / miningTime;
+
+    public delegate void OnEmpty();
+    public OnEmpty onEmpty;
+
+    private void Start()
+    {
+        resourcesRemaining = Random.Range(minResources, maxResources);
+    }
+
+    private void Update()
+    {
+        if (!beingMined && countdown > 0)
+            countdown -= Time.deltaTime;
+
+        beingMined = false;
+    }
+
+    public void EnterMine(Miner m) 
+    {
+        currentMiner = m;
+    }
+
+    public void ExitMine()
+    {
+        currentMiner = null;
+    }
+
+    public void MineResource() 
+    {
+        if (resource == null || resourcesRemaining == 0)
+            return;
+
+        beingMined = true;
+
+        countdown += Time.deltaTime * currentMiner.MiningSpeed;
+
+        if (countdown >= miningTime)
+        {
+            countdown = 0;
+
+            currentMiner.ReceiveResource(resource);
+            resourcesRemaining--;
+        }
+        
+        if (resourcesRemaining == 0)
+        {
+            gameObject.SetActive(false);
+            onEmpty?.Invoke();
+        }
+    }
+}
---
+++ Miner.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleInventory;
+
+public class Miner : MonoBehaviour
+{
+    [SerializeField] Inventory inventory = null;
+
+    [SerializeField] Mineable currentMine = null;
+    [SerializeField] MiningUI mineUI = null;
+
+    [SerializeField] private float miningSpeed = 2.0f;
+    public float MiningSpeed => miningSpeed;
+
+    public delegate void OnEnterMine(Mineable m);
+    public OnEnterMine onEnterMine;
+
+    public delegate void OnExitMine();
+    public OnExitMine onExitMine;
+
+    private void Start()
+    {
+        mineUI.InitializeUI();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Mine"))
+        {
+            Mineable m;
+            
+            if (other.TryGetComponent(out m))
+                EnterMine(m);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Mine"))
+            ExitMine();
+    }
+
+    private void EnterMine(Mineable m) 
+    {
+        currentMine = m;
+
+        currentMine.EnterMine(this);
+
+        currentMine.onEmpty += ExitMine;
+
+        onEnterMine?.Invoke(currentMine);
+    }
+
+    private void ExitMine() 
+    {
+        if (currentMine) 
+        {
+            currentMine.onEmpty -= ExitMine;
+
+            currentMine.ExitMine();
+        }
+
+        currentMine = null;
+
+        onExitMine?.Invoke();
+    }
+
+    private bool CursorOverCurrentMine()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, .5f, out hit, 100f))
+        {
+            Mineable m;
+            if (hit.collider.TryGetComponent(out m))
+                return m == currentMine;
+        }
+        return false;
+    }
+
+    #region Public Methods
+
+    public void MineCurrent() 
+    {
+        if (currentMine && CursorOverCurrentMine())
+            currentMine.MineResource();
+    }
+
+    public void ReceiveResource(Item i) 
+    {
+        if (inventory)
+            inventory.AddToInventory(i);
+        else
+            Debug.LogError("Theres no inventory on " + name);
+    }
+    #endregion
+}
