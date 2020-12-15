02ed9984407ac848b7afa3fbc1d26aa512b71298
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2020-12-15 22:28:23 +0330

    Made the Miner the primary class in mining.
    Before, Mineables did more than what they should have.
    
    Now the miner has one more variable (countdown), another event (OnProgressChange), and the routines change so that it is responsible for taking resources from a mine (as opposed to the mine 'giving' it's resources to the Miner via Miner.ReceiveResource(r). 
    
    Where Mineable used Update to reduce mining progress while not being mined, the miner now holds a coroutine to reduce progress when leaving a mine.
    
    The routine MineCurrent is probably the bulkiest and could be broken into other routines, however it's called every frame that the mineKey is down. It's got a few parts:
    
    1. Checking that the mouse is over a Mineable and that it's within distance of the Miner.
    2. Increasing Mining Progress.
    3. Extracting resources when progress is full
    3. a) Leaving the mine if empty.
    4. Fire the Mining Progress event.

--- Scripts/Mining/Miner.cs
+++ Scripts/Mining/Miner.cs
@@ -1,84 +1,135 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SimpleInventory;
 
-public class Miner : MonoBehaviour
+namespace SimpleInventory
 {
-    #region Variables
-    [SerializeField] Inventory inventory = null;
-
-    [SerializeField] Mineable currentMine = null;
-
-    [SerializeField] private float miningSpeed = 2.0f;
-
-    [SerializeField, Header("If within this distance of a Mineable, the miner can mine that resource.")]
-    private float validDistance = 5.0f;
-    #endregion
-
-    #region Properties and Events
-    public float MiningSpeed => miningSpeed;
-
-    public delegate void OnEnterMine(Mineable m);
-    public OnEnterMine onEnterMine;
-
-    public delegate void OnExitMine();
-    public OnExitMine onExitMine;
-    #endregion
-
-    private void EnterMine(Mineable m)
+    public class Miner : MonoBehaviour
     {
-        currentMine = m;
+        #region Variables
+        [SerializeField] Inventory inventory = null;
 
-        currentMine.onEmpty += ExitMine;
+        [SerializeField] Mineable currentMine = null;
 
-        onEnterMine?.Invoke(currentMine);
-    }
+        [SerializeField] LayerMask mineableMask = 1 << 9;
 
-    #region Public Methods
-    //Called by Input Manager
-    public void MineCurrent()
-    {
-        //----------------If cursor over a Mineable-----------------
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        [SerializeField] private float miningSpeed = 2.0f;
 
-        //if (Physics.SphereCast(ray, .5f, out hit, 100f))
-        if (Physics.Raycast(ray, out hit, 100f))
+        [SerializeField, Header("If within this distance of a Mineable, the miner can mine that resource.")]
+        private float validDistance = 5.0f;
+
+        private float countdown = 0;
+
+        private IEnumerator mineCountdown = null;
+        #endregion
+
+        #region Events
+        public delegate void OnMiningProgressChange(float progress);
+        public OnMiningProgressChange onMiningProgressChange;
+
+        public delegate void OnEnterMine(Mineable m);
+        public OnEnterMine onEnterMine;
+
+        public delegate void OnExitMine();
+        public OnExitMine onExitMine;
+        #endregion
+
+        #region Private Methods
+        private void Start()
         {
-            Mineable m;
+            mineCountdown = DoMineCountdown(0);
+        }
 
-            //If within distance of a valid mineable object
-            if (hit.collider.TryGetComponent(out m)
-                && Vector3.Distance(transform.position, hit.point) <= validDistance)
+        private void EnterMine(Mineable m)
+        {
+            if (currentMine == m)
+                return;
+
+            currentMine = m;
+
+            onEnterMine?.Invoke(currentMine);
+        }
+
+        private void ReceiveResource(Item i)
+        {
+            if (inventory)
+                inventory.AddToInventory(i);
+            else
+                Debug.LogError("Theres no inventory on " + name +". Add an Inventory component to their Miner Component in the inspector.");
+        }
+
+        private void LeaveEmptyMine()
+        {
+            currentMine = null;
+
+            onExitMine?.Invoke();
+        }
+
+        private IEnumerator DoMineCountdown(float mineTime)
+        {
+            while (countdown > 0)
             {
-                EnterMine(m);
-
-                m.MineResource(this);
+                countdown -= Time.deltaTime;
+                onMiningProgressChange?.Invoke(countdown / mineTime);
+                yield return null;
             }
         }
-    }
+        #endregion
 
-    public void ExitMine()
-    {
-        if (currentMine)
+        #region Public Methods
+        //Called by Input Manager
+        public void MineCurrent(GameObject objectThatTheMouseIsOver)
         {
-            currentMine.onEmpty -= ExitMine;
+           Mineable m;
+
+            if (!objectThatTheMouseIsOver.TryGetComponent(out m)
+                || Vector3.Distance(transform.position, objectThatTheMouseIsOver.transform.position) > validDistance)
+            {
+                ExitMine();
+                return;
+            }
+
+            //--------Entering a new mine------------
+            if (currentMine == null)
+            {
+                StopCoroutine(mineCountdown);
+                mineCountdown = DoMineCountdown(m.MiningTime);
+
+                EnterMine(m);
+            }
+            //---------------------------------------
+
+            //Mining progress
+            countdown += Time.deltaTime * miningSpeed;
+
+            //----------Upon achieving full mining progress---------
+            if (countdown >= m.MiningTime)
+            {
+                countdown = 0;
+
+                m.MineResource();
+                ReceiveResource(m.Resource);
+
+                if (!m.HasResources)
+                {
+                    //Mine is empty.
+                    LeaveEmptyMine();
+                }
+            }
+            //---------------------------------------------------
+
+            onMiningProgressChange?.Invoke(countdown / m.MiningTime);
         }
 
-        currentMine = null;
+        public void ExitMine()
+        {
+            if (currentMine == null)
+                return;
 
-        onExitMine?.Invoke();
-    }
+            currentMine = null;
 
-    //Called by the mine
-    public void ReceiveResource(Item i)
-    {
-        if (inventory)
-            inventory.AddToInventory(i);
-        else
-            Debug.LogError("Theres no inventory on " + name);
+            StartCoroutine(mineCountdown);
+        }
+        #endregion
     }
-    #endregion
 }
-
