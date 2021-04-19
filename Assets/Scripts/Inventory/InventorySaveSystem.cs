e0d40baa388a4bbd49e5b7bb4ae719f1d98d0bc4
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2021-04-19 15:04:08 +0430

    Save system added

---
+++ Assets/Scripts/Inventory/InventorySaveSystem.cs
@@ -0,0 +1,87 @@
+﻿using System.IO; //For reading and writing
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleInventory 
+{
+    public class InventorySaveSystem : MonoBehaviour
+    {
+        [SerializeField] private Inventory inventoryToSave = null;
+
+        private static Dictionary<int, Item> allItemCodes = new Dictionary<int, Item>();
+
+        private static int HashItem(Item item) => Animator.StringToHash(item.ItemName);
+
+        const char SPLIT_CHAR = '_';
+
+        private static string FILE_PATH ="NULL!";
+
+        private void Awake()
+        {
+            FILE_PATH = Application.persistentDataPath + "/Inventory.txt";
+
+            CreateItemDictionary();
+        }
+
+        private void OnDisable()
+        {
+            SaveInventory();
+        }
+
+        private void CreateItemDictionary() 
+        {
+            Item[] allItems = Resources.FindObjectsOfTypeAll<Item>();
+
+            foreach (Item i in allItems) 
+            {
+                int key = HashItem(i);
+
+                if (!allItemCodes.ContainsKey(key))
+                    allItemCodes.Add(key, i);
+            }
+        }
+
+        public void SaveInventory() 
+        {
+            using (StreamWriter sw = new StreamWriter(FILE_PATH))
+            {
+                foreach (KeyValuePair<Item, int> kvp in inventoryToSave.GetInventory)
+                {
+                    Item item = kvp.Key;
+                    int count = kvp.Value;
+
+                    string itemID = HashItem(item).ToString();
+
+                    sw.WriteLine(itemID + SPLIT_CHAR + count);                    
+                }
+            }
+        }
+
+        internal Dictionary<Item, int> LoadInventory() 
+        {
+            if (!File.Exists(FILE_PATH))
+            {
+                Debug.LogWarning("The file you're trying to access doesn't exist. (Try saving an inventory first).");
+                return null;
+            }
+
+            Dictionary<Item, int> inventory = new Dictionary<Item, int>();
+
+            string line = "";
+
+            using (StreamReader sr = new StreamReader(FILE_PATH)) 
+            {
+                while ((line = sr.ReadLine()) != null) 
+                {
+                    int key = int.Parse( line.Split(SPLIT_CHAR)[0] );
+                    Item item = allItemCodes[key];
+                    int count = int.Parse(line.Split(SPLIT_CHAR)[1]);
+
+                    inventory.Add(item, count);
+                }
+            }
+
+            return inventory;
+        }
+    } 
+}
