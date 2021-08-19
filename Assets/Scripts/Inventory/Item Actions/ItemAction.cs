68d4160c550c3609993f2546e0a02d377066c752
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2021-08-19 23:47:24 +0200

    Delete ItemAction.cs

--- Assets/Scripts/Inventory/Item Actions/ItemAction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public abstract class ItemAction : DynamicAction<SimpleInventory.Item>
-{
-    public override abstract UnityAction Action(SimpleInventory.Item i, int n, object extra = null);
-}
