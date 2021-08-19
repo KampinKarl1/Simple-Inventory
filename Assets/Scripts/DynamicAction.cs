80e196c5116943dbade587c2ba80dc0ea504d080
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2021-08-19 22:15:23 +0200

    Removed nested classes from DynamicAction
    trying to remove stupidity from brain.

---
+++ Assets/Scripts/DynamicAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class DynamicAction<T> : ScriptableObject
+{
+#if UNITY_EDITOR
+    [SerializeField, Multiline] private string description;
+#endif
+
+    public abstract UnityAction Action(T i, int n, object extra = null);
+}
